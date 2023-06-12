003aede688c7156c1689ecb3f650a3302740765c
James Hallberg <Grebllah81788@gmail.com>
2023-06-12 14:49:23 -0400

    complete scope lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your solution in this file!
+var customerName = 'bob'
+upperCaseCustomerName = () => {
+    customerName = customerName.toUpperCase()
+}
+setBestCustomer = () => {
+    bestCustomer = 'not bob'
+}
+overwriteBestCustomer = () => {
+    bestCustomer = 'maybe bob'
+}
+const leastFavoriteCustomer = 'james'
+
+changeLeastFavoriteCustomer = () => {
+    leastFavoriteCustomer = 'anyone else'
+}
